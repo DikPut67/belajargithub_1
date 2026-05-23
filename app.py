9d5bf74e9fabc4e4f69014c7e74199a011ed620d
DikPut67 <dikaputra2910@gmail.com>
2026-05-23 10:32:21 +0700

    Refactor navigation structure in app.py

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
-your_repository/
-├── create_account.py
-├── learn.py
-├── manage_account.py
-├── streamlit_app.py
-└── trial.py
+pages = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
